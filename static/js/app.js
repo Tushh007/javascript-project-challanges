13d2ffa9471dc2334eb8cbb09642afec3069df98
Tushar Langer <tushar.langer@gmail.com>
2021-02-20 15:25:09 +0530

    cat generator challange completed

--- static/js/app.js
+++ static/js/app.js
@@ -14,14 +14,27 @@ const ageInDays = () => {
 };
 
 const reset = () => {
-    document.getElementById('ageInDays').remove();
-}
+  document.getElementById('ageInDays').remove();
+};
 
 document.querySelector('#age-btn').addEventListener('click', () => {
   ageInDays();
 });
 
-
 document.querySelector('#age-reset-btn').addEventListener('click', () => {
-    reset();
-  });
+  reset();
+});
+
+// Challange 2: Cat Generator
+
+const generateCat = () => {
+  let image = document.createElement('img');
+  let div = document.getElementById('flex-cat-gen');
+  image.src =
+    'https://thecatapi.com/api/images/get?format=src&type=gif&size=small';
+  div.appendChild(image);
+};
+
+document.querySelector('#cat-gen-btn').addEventListener('click', () => {
+  generateCat();
+});
